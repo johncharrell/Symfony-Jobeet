894beefac3e353086edcbdd4c230629fc0cec55e
Chris Harrell <Chris.Harrell@skillsnet.com>
2011-09-14 16:45:29 -0500

    additional changes

--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -10,8 +10,19 @@
  * @author     Your name here
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-class JobeetJob extends BaseJobeetJob
-{
+	class JobeetJob extends BaseJobeetJob
+	{
+
+	public function save(Doctrine_Connection $conn = null)
+	{
+		if ($this->isNew() && !$this->getExpiresAt())
+		{
+			$now = $this->getCreatedAt() ? $this->getDateTimeObject('created_at')->format('U') : time();
+			$this->setExpiresAt(date('Y-m-d H:i:s', $now + 86400 * sfConfig::get('app_active_days')));
+		}
+		return parent::save($conn);
+	}
+
 	public function getCompanySlug()
 	{
 	  return Jobeet::slugify($this->getCompany());
